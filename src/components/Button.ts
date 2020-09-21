98807e39924b5b4075dc77b957d83e3feea24766
Dixon Minnick <dminnick@rxsense.com>
2020-09-21 16:35:10 -0700

    more setup

---
+++ src/components/Button.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`;
+
+export default Button;
